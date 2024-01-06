f07c7fa2d499d126eace9559866060ecfab1271f
shashankpantiitbhilai <shashankpant94115@gmail.com>
2024-01-06 13:24:58 +0530

    adding authentication page

---
+++ frontend/src/components/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Login from "./Login.jsx";
+ function App() {
+    return (
+        <Login/>
+    
+    );
+  }
+  
+  export default App;
